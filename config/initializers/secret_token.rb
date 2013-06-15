eb6cf42561244dc77c8af2cb3f842eeca27dd89e
Peregrinator <bob.burbach@gmail.com>
2013-06-15 11:24:52 -0700

    new rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OusdCommunityPartners::Application.config.secret_key_base = '4c2400ffc099c7d6734b545d58e0ed3fc1213e7626c702811f603b7602438e624ffc8899daf22c5e44ce4dc6f4f6b26f6a14def845aef763b9dab4c8add5d957'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OusdCommunityPartners::Application.config.session_store :cookie_store, key: '_ousd_community_partners_session'
