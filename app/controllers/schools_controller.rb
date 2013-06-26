a52e7480fd18931d6a051d204e92c7f93388ba20
Peregrinator <bob.burbach@gmail.com>
2013-06-25 18:45:17 -0700

    Allow creation and edit of schools. Add region to index page.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -6,4 +6,32 @@ class SchoolsController < ApplicationController
   def show
     @school = School.includes(:community_partners).find(params[:id])
   end
+
+  def new
+    @school = School.new
+  end
+
+  def create
+    @school = School.new(school_params)
+    @school.save
+
+    redirect_to school_path(@school)
+  end
+
+  def edit
+    @school = School.find(params[:id])
+  end
+
+  def update
+    @school = School.find(params[:id])
+    @school.update_attributes(school_params)
+
+    redirect_to school_path(@school)
+  end
+
+  private
+
+  def school_params
+    params.require(:school).permit(:name)
+  end
 end
