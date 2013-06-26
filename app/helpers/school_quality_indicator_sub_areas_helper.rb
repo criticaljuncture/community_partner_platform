208b231a2f7ffc4ac6c0026e11ba9b5e573dcb3d
Peregrinator <bob.burbach@gmail.com>
2013-06-25 20:04:24 -0700

    build out schools page a bit more

--- app/helpers/school_quality_indicator_sub_areas_helper.rb
+++ app/helpers/school_quality_indicator_sub_areas_helper.rb
@@ -1,2 +1,20 @@
 module SchoolQualityIndicatorSubAreasHelper
+  def school_quality_indicator_sub_area_display(school)
+    school_sub_area_count = school.school_quality_indicator_sub_areas.uniq.count
+    sub_area_count = SchoolQualityIndicatorSubArea.count
+
+    ratio = school_sub_area_count.to_f / sub_area_count.to_f
+
+    display_class = ratio > 0.7 ? 'good' : (ratio > 0.5 ? 'satisfactory' : 'bad')
+
+    content_tag(:span,
+                "#{school_sub_area_count}/#{sub_area_count}",
+                class: display_class)
+  end
+
+  def community_partner_count_for_indictor(indicator, school) 
+    count = school.community_partners.select{|cp| cp.school_quality_indicator_sub_area_id == indicator.id}.count
+
+    content_tag(:span, count, class: count > 0 ? 'satisfactory' : 'bad')
+  end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,5 +2,7 @@ class School < ActiveRecord::Base
   has_many :community_partners
   has_many :organizations, through: :community_partners
 
+  has_many :school_quality_indicator_sub_areas, through: :community_partners
+
   belongs_to :region
 end
