eb52f68d256ccb5ee21f86b8a3906bfe4d27ab71
Peregrinator <bob.burbach@gmail.com>
2013-09-27 17:20:25 -0700

    Basic role tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,9 +4,14 @@ FactoryGirl.define do
     last_name 'Doe'
     email { "#{first_name}.#{last_name}@example.com".downcase }
 
-    password 'supers3c3t!'
-    password_confirmation 'supers3c3t!'
+    password 'sup3rs3cr3t!'
+    password_confirmation 'sup3rs3cr3t!'
 
+    roles { [Role.first] }
+  end
+
+  factory :organization_member, parent: :user do
     roles { [Role.find_by_identifier(:organization_member)] }
+    organization_id 1
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,20 +30,37 @@ describe User do
     end
   end
 
+  context "with role of" do
+    describe ":organization member" do
+      let(:user) { build(:organization_member) }
+
+      it "is invalid without an organization id" do
+        user.organization_id = nil
+        
+        expect( user ).to be_invalid
+      end
+
+      it "is valid with an organization id" do
+        expect( user ).to be_valid
+      end
+    end
+  end
+
   describe "#role?" do
     let(:user) { build(:user) }
 
     it "returns true when the user only possesses the given role" do
+      user.stub(:roles).and_return( [Role.find_by_identifier(:organization_member)] )
       expect( user.role?(:organization_member) ).to eq(true)
     end
 
     it "returns true when the user possesses the given role (among many)" do
       user.stub(:roles).and_return( [Role.find_by_identifier(:school_manager), Role.find_by_identifier(:district_manager)] )
-
       expect( user.role?(:district_manager) ).to eq(true)
     end
 
     it "returns false when the user doesn not possess the given role" do
+      user.stub(:roles).and_return( [Role.find_by_identifier(:school_manager)] )
       expect( user.role?(:admin) ).to eq(false)  
     end
   end
