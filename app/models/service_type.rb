af3378b4fe5aa60c85b7045add8d7550fec7ca8f
Peregrinator <bob.burbach@gmail.com>
2013-10-01 10:53:32 -0700

    Don't have the same association twice but different...

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -1,10 +1,7 @@
 class ServiceType < ActiveRecord::Base
   has_many :quality_element_service_types
   has_many :quality_elements, through: :quality_element_service_types
-
-  has_many :community_partner_quality_element_service_types
-  has_many :quality_elements, through: :community_partner_quality_element_service_types
-
+ 
   has_many :community_partners, through: :quality_elements
 
   has_many :schools, through: :community_partners
