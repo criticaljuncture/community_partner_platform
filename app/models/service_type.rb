ebd7b6ce07de5c7a95851c94161328a6e6719d73
Peregrinator <bob.burbach@gmail.com>
2013-10-01 10:51:08 -0700

    Bug fix for service types - should be revisited.

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -2,8 +2,11 @@ class ServiceType < ActiveRecord::Base
   has_many :quality_element_service_types
   has_many :quality_elements, through: :quality_element_service_types
 
-  has_many :community_partnership_service_types
-  has_many :community_partners, through: :community_partnership_service_types
+  has_many :community_partner_quality_element_service_types
+  has_many :quality_elements, through: :community_partner_quality_element_service_types
+
+  has_many :community_partners, through: :quality_elements
+
   has_many :schools, through: :community_partners
   has_many :organizations, through: :community_partners
 
