256c842c03439fde343cfec10225f32ef538770b
Peregrinator <bob.burbach@gmail.com>
2013-11-09 08:48:24 -0800

    Add subdomain customization via I18n

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,11 @@ module OusdCommunityPartners
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'forms', '{**}')]
     config.autoload_paths += Dir[Rails.root.join('app', 'audits', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('lib')]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
@@ -28,7 +29,8 @@ module OusdCommunityPartners
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     config.assets.precompile += %w(
       *.png *.jpg *.jpeg *.gif
       *.eot *.svg *.ttf *.woff
