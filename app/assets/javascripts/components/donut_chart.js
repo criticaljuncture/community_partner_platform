7beb53b0e793607394343c60db973da221316f4d
Peregrinator <bob.burbach@gmail.com>
2013-11-19 10:56:34 -0800

    Update donut chart so that it contains a count in the middle. Update helper so that it creates the donut chart in the proper css component form.

--- app/assets/javascripts/components/donut_chart.js
+++ app/assets/javascripts/components/donut_chart.js
@@ -1,18 +1,29 @@
-OusdCommunityPartners.DonutChartComponent = Ember.Component.extend({
-  tagName: 'svg',
+CommunityPartnerPlatform.DonutChartComponent = Ember.Component.extend({
+  tagName: 'div',
   templateName: 'donut-chart',
 
+  graphElement: '.graph',
+  donutRadius: 50,
+  donutThickness: 20,
+
+  width: 100,
+  height: 100,
+
   didInsertElement: function(){
-    var width = this.get('width');
-    var height = this.get('height');
+    var width = this.get('width'),
+        height = this.get('height'),
+        graphElement = this.get('graphElement'),
+        radius = this.get('donutRadius'),
+        thickness = this.get('donutThickness')
+        Donut = this;
+
 
     var chartData = JSON.parse( this.get('chartData') );
     var chartColorNames = JSON.parse( this.get('chartColorNames') );
 
-    var radius = Math.min(width, height) / 2;
     var arc = d3.svg.arc()
                 .outerRadius(radius)
-                .innerRadius(radius-20);
+                .innerRadius(radius-thickness);
 
     var pie = d3.layout.pie()
                 .sort(null)
@@ -20,19 +31,47 @@ OusdCommunityPartners.DonutChartComponent = Ember.Component.extend({
 
     var id = this.$().attr('id');
     var svg = d3.select("#"+id)
+                .select(graphElement)
                 .append("svg")
+                  .attr("class", "donut-chart")
                   .attr("width", width)
                   .attr("height", height)
                   .append("g")
                     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    var g = svg.selectAll(".arc")
-                  .data(pie(chartData))
-                  .enter()
-                    .append("g")
-                      .attr("class", function(d,i) { return "arc " + chartColorNames[i] });
+    var arcs = svg.selectAll(".arc")
+                  .data(pie(chartData));
 
-    g.append("path")
-      .attr("d", arc);
-  }
+    arcs.enter()
+      .append("g")
+        .append("path")
+          .attr("d", arc)
+          .attr("class", function(d,i) { return "arc " + chartColorNames[i] });
+
+    svg.append("text")
+      .attr("class", "chart_count " + chartColorNames[0])
+      .attr("dy", ".35em")
+      .attr("text-anchor", "middle")
+      .text(chartData[0].count);
+
+    d3.select("#"+id)
+        .select(graphElement)
+        .select("svg")
+          .attr("data-chart-info", Donut.get('chartHover'))
+
+    add_tipsy(graphElement + " svg", {gravity: 'w',
+                                      title: function(){
+                                        return this.getAttribute('data-chart-info')
+                                      }
+    });
+  },
+
+  chartHover: function() {
+    data = JSON.parse( this.get('chartData') );
+
+    complete = data[0].count;
+    total = data[1].count + complete;
+
+    return Math.round(complete/total*100) + "% (" + complete + "/" + total + ")"
+  }.property('chartData'),
 });
--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -1,29 +1,42 @@
 module ChartHelper
-  def completeness_donut(complete, total)
-    html = []
-    html << "<script type='text/x-handlebars' data-template-name='application'>"
-    html << donut_chart(complete, total)
-    html << "</script>"
-
-    html.join("\n").html_safe
+  def completeness_donut(title, complete, total)
+    donut_chart(title, complete, total).html_safe
   end
 
   private
 
-  def donut_chart(complete, total)
-    width = 300
-    height = 100
-    class_names = 'donut'
+  def donut_chart(title, complete, total)
+    width = 120
+    height = 120
+    radius = 60
+    thickness = 20
+
+    class_names = ''
+
     chart_data = [
-      {label: 'complete', count: complete},
-      {label: 'total',    count: total}
+      {label: 'complete',   count: complete},
+      {label: 'incomplete', count: total-complete}
     ].to_json
-    chart_color_names = ["purple", "gray-30"].to_json.html_safe
+
+    complete_color = color_for_completeness(complete.to_f/total.to_f)
+    incomplete_color = "gray-30"
+
+    chart_color_names = [complete_color, incomplete_color].to_json.html_safe
 
     "{{donut-chart  width=#{width}
                     height=#{height}
                     classNames='#{class_names}'
                     chartData='#{chart_data}'
-                    chartColorNames='#{chart_color_names}'}}".html_safe
+                    chartColorNames='#{chart_color_names}'
+                    donutRadius='#{radius}'
+                    donutThickness='#{thickness}'
+                    title='#{title}'
+    }}".html_safe
+  end
+
+  def color_for_completeness(completeness)
+    return "green"    if completeness > 0.75
+    return "orange"   if completeness > 0.40
+    return "red"
   end
 end
