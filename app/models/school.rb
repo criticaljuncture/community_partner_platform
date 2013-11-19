a819f9aa97f6f0c735b37db5977d43387d8f86b1
Peregrinator <bob.burbach@gmail.com>
2013-11-19 10:57:28 -0800

    Add methods for counts of various related models via community partnerships

--- app/models/school.rb
+++ app/models/school.rb
@@ -14,10 +14,30 @@ class School < ActiveRecord::Base
   default_scope -> { where(direct_funded_charter_school: false) }
 
   def quality_elements
-    community_partners.map{|cp| cp.quality_elements}.flatten.uniq
+    @quality_elements ||= community_partners.map{|cp| cp.quality_elements}.flatten.uniq
   end
 
   def service_types
-    community_partners.map{|cp| cp.service_types}.flatten.uniq
+    @service_types ||= community_partners.map{|cp| cp.service_types}.flatten.uniq
+  end
+
+  def student_populations_with_partnerships
+    @student_populations_with_partnerships ||= community_partners.map{|cp| cp.student_population}.compact.uniq
+  end
+
+  def ethnicity_culture_groups_with_partnerships
+    @ethnicity_culture_groups_with_partnerships ||= community_partners.map{|cp| cp.ethnicity_culture_groups}.flatten.compact.uniq
+  end
+
+  def demographic_groups_with_partnerships
+    @demographic_groups_with_partnerships ||= community_partners.map{|cp| cp.demographic_groups}.flatten.compact.uniq
+  end
+
+  def service_days_with_partnerships
+    @service_days_with_partnerships ||= community_partners.map{|cp| cp.days}.flatten.compact.uniq
+  end
+
+  def service_times_with_partnerships
+    @service_times_with_partnerships ||= community_partners.map{|cp| cp.service_times}.flatten.compact.uniq
   end
 end
