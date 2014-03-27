5be053a9e5aa627dfed2bad1cfa5414d78fee691
Peregrinator <bob.burbach@gmail.com>
2014-03-27 12:36:47 -0700

    Fix migrations

--- db/migrate/20140324232100_rename_community_partner_tables_to_community_program.rb
+++ db/migrate/20140324232100_rename_community_partner_tables_to_community_program.rb
@@ -1,6 +1,6 @@
 class RenameCommunityPartnerTablesToCommunityProgram < ActiveRecord::Migration
   def change
-    rename_table :community_partnerships, :community_programs
+    rename_table :community_partners, :community_programs
     rename_table :community_partner_quality_elements, :community_program_quality_elements
     rename_table :community_partner_quality_element_service_types, :community_program_quality_element_service_types
     rename_table :community_partnership_demographic_groups, :community_program_demographic_groups
--- db/migrate/20140324232135_rename_community_partner_columns_to_community_program.rb
+++ db/migrate/20140324232135_rename_community_partner_columns_to_community_program.rb
@@ -6,7 +6,6 @@ class RenameCommunityPartnerColumnsToCommunityProgram < ActiveRecord::Migration
     rename_column :community_program_grade_levels, :community_partner_id, :community_program_id
     rename_column :community_program_service_days, :community_partner_id, :community_program_id
     rename_column :community_program_service_times, :community_partner_id, :community_program_id
-    rename_column :community_program_quality_elements, :community_partner_quality_element_id, :community_program_quality_element_id
     rename_column :community_program_quality_element_service_types, :community_partner_quality_element_id, :community_program_quality_element_id
   end
 end
