5a34a813b2f00b6ce34b2d07c1da89cc31cf4c51
Peregrinator <bob.burbach@gmail.com>
2014-04-03 17:06:50 -0700

    * Fix authorization checks to no longer use authorize_resource, but to check individually in each method -> less wizardry * Update flash messages to use I18n and add error flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,27 @@
 class UsersController < ApplicationController
-  authorize_resource :user
   skip_before_filter :require_no_authentication
 
   def index
     @active_users = User.accessible_by(current_ability).active.sort_by(&:last_name).sort_by(&:first_name)
     @inactive_users = User.accessible_by(current_ability).inactive.sort_by(&:last_name).sort_by(&:first_name)
+
+    authorize! :index, User
   end
 
   def show
-    @user = User.accessible_by(current_ability).find(params[:id])
+    @user = User.find(params[:id])
+    authorize! :show, @user
   end
 
   def new
     @user = User.new
+    authorize! :create, @user
   end
 
   def create
     @user = User.new(user_params)
+    authorize! :create, @user
+
     begin
       User.transaction do
         primary_role = user_params[:primary_role].present? ? user_params[:primary_role].to_i : nil
@@ -27,24 +32,30 @@ class UsersController < ApplicationController
 
         @user.admin_creation = can?(:create, User)
         @user.save!
-     
-        flash.notice = "#{@user.full_name} has been created with role '#{@user.primary_role.name}', but not invited to the Community Partner Platform." 
+
+        flash.notice = t('users.flash_messages.create.success',
+                         name: @user.full_name,
+                         role: @user.primary_role.name)
         redirect_to users_path(active_tab: 'inactive')
       end
     rescue ActiveRecord::RecordInvalid
+      flash.now[:notice] = t('users.flash_messages.save.failure',
+                            errors: @user.errors.count)
       render action: :new
     end
   end
 
   def edit
-    @user = User.accessible_by(current_ability).find(params[:id])
+    @user = User.find(params[:id])
+    authorize! :update, @user
   end
 
   def update
-    @user = User.accessible_by(current_ability).find(params[:id])
+    @user = User.find(params[:id])
+    authorize! :update, @user
 
     primary_role = user_params[:primary_role].present? ? user_params[:primary_role].to_i : nil
-    
+
     begin
       User.transaction do
         if primary_role
@@ -56,25 +67,35 @@ class UsersController < ApplicationController
 
         @user.update_attributes!(user_params.except(:primary_role))
 
+        flash.notice = t('users.flash_messages.update.success',
+                         name: @user.full_name)
+
         redirect_to action: :index
       end
     rescue ActiveRecord::RecordInvalid
+      flash.now[:notice] = t('users.flash_messages.save.failure',
+                            errors: @user.errors.count)
+
       render action: :edit
     end
   end
 
   def send_invitation
-    @user = User.accessible_by(current_ability).find(params[:id])
+    @user = User.find(params[:id])
+    authorize! :send_invitation, @user
+
     @user.invite!(current_user)
 
-    flash.notice = "Sent invitation to #{@user.full_name} (#{@user.email})."
+    flash.notice = t('users.flash_messages.invitation.sent',
+                     name: @user.full_name,
+                     email: @user.email)
     redirect_to users_path(active_tab: 'inactive')
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:first_name, 
+    params.require(:user).permit(:first_name,
                                  :last_name,
                                  :email,
                                  :phone_number,
