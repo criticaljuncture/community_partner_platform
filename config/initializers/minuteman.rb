61b1a92c643e034a1c3f5c8d0a8a81e3edd2a4d9
Peregrinator <bob.burbach@gmail.com>
2014-07-02 10:22:26 -0700

    namespace minuteman by environment

--- config/initializers/minuteman.rb
+++ config/initializers/minuteman.rb
@@ -1,4 +1,6 @@
+namespace = "minuteman_#{Rails.env}".to_sym
+
 Minuteman::Rails.configure do |c|
   c.silent = true
-  c.redis = Redis::Namespace.new(:minuteman, :redis => $redis)
+  c.redis = Redis::Namespace.new(namespace, :redis => $redis)
 end
