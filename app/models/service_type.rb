3f342b5345ac4217a55c6930f8e179263fe65671
Peregrinator <bob.burbach@gmail.com>
2014-07-02 12:35:59 -0700

    Update how we calculate the schools, organizations and community programs for a given service type. Before we were counting all the schools for a service type across all it’s associated quality elements.

--- app/models/service_type.rb
+++ app/models/service_type.rb
@@ -2,11 +2,32 @@ class ServiceType < ActiveRecord::Base
   has_many :quality_element_service_types
   has_many :quality_elements, through: :quality_element_service_types
 
-  has_many :community_programs, through: :quality_elements
-
-  has_many :schools, through: :community_programs
-  has_many :organizations, through: :community_programs
+  has_many :community_program_quality_element_service_types
+  has_many :community_program_quality_elements,
+    through: :community_program_quality_element_service_types
 
   validates :name, presence: true
   validates :quality_elements, presence: true
+
+  def quality_element_groups
+    community_program_quality_elements.group_by(&:quality_element_id)
+  end
+
+  def community_programs_for_quality_element_service_type(quality_element)
+    groups = quality_element_groups[quality_element.id]
+
+    if groups
+      groups.map{|g| g.community_program}
+    else
+      []
+    end
+  end
+
+  def schools_for_quality_element_service_type(quality_element)
+    community_programs_for_quality_element_service_type(quality_element).compact.map{|cp| cp.school}.uniq
+  end
+
+  def organizations_for_quality_element_service_type(quality_element)
+    community_programs_for_quality_element_service_type(quality_element).compact.map{|cp| cp.organization}.uniq
+  end
 end
