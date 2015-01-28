f014c4d24788b5d0972dada144233c4a7ca9d19a
Peregrinator <bob.burbach@gmail.com>
2015-01-27 23:25:16 -0800

    Only require secure cookies in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,6 +3,6 @@
 OusdCommunityPartners::Application.config.session_store :cookie_store,
   key: '_ousd_community_partners_session',
   # Only send cookie over SSL when in production mode
-  secure: true,
+  secure: Rails.env.production? ? true : false,
   # Don't allow Javascript to access the cookie (mitigates cookie-based XSS exploits)
   httponly: true
