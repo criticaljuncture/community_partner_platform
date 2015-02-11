3ca9a1c51d88764a1de9688db018e616b2bfbfd5
Peregrinator <bob.burbach@gmail.com>
2015-02-11 00:13:06 -0800

    Refactor helper to be less dependent on a school being passed in.

--- app/helpers/quality_elements_helper.rb
+++ app/helpers/quality_elements_helper.rb
@@ -1,14 +1,13 @@
 module QualityElementsHelper
-  def quality_elements_coverage_display(school)
-    school_sub_area_count = school.quality_elements.count
+  def quality_elements_coverage_display(quality_element_count)
     sub_area_count = QualityElement.accessible_by(current_ability).count
 
-    ratio = school_sub_area_count.to_f / sub_area_count.to_f
+    ratio = quality_element_count.to_f / sub_area_count.to_f
 
     display_class = ratio > 0.7 ? 'good' : (ratio > 0.5 ? 'satisfactory' : 'bad')
 
     content_tag(:span,
-                "#{school_sub_area_count}/#{sub_area_count}",
+                "#{quality_element_count}/#{sub_area_count}",
                 class: display_class)
   end
 
