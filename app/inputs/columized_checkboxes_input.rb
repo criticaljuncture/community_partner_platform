f1b681cd924308398c42d23e484d9d9ab8e94207
Peregrinator <bob.burbach@gmail.com>
2015-09-01 17:28:52 -0700

    Remove old cruft from formtastic days

--- app/inputs/columized_checkboxes_input.rb
+++ app/inputs/columized_checkboxes_input.rb
@@ -9,7 +9,7 @@ class ColumizedCheckboxesInput < SimpleForm::Inputs::CollectionCheckBoxesInput
 
     column_html = input_options[:column_html] || {class: 'col-md-4'}
     choice_groups = []
-
+    
     if collection.present?
       groups_of = (collection.length.to_f / columns).ceil
 
@@ -30,31 +30,3 @@ class ColumizedCheckboxesInput < SimpleForm::Inputs::CollectionCheckBoxesInput
     choice_groups.join("\n").html_safe
   end
 end
-
-#   def to_html
-#     choice_groups = []
-#
-#     raise "You must provide a number of columns! eg. {columns: 3}" unless input_options[:columns]
-#
-#     groups_of = (collection.length.to_f / input_options[:columns]).ceil
-#
-#     if collection.present?
-#       collection.in_groups_of(groups_of, false) do |choices|
-#         html = ["<div class='checkbox-column'>"]
-#         html << choices.map do |choice|
-#                   choice_html(choice)
-#                 end.join("\n").html_safe
-#         html << "</div>"
-#         choice_groups << html.join("\n").html_safe
-#       end
-#     end
-#
-#     control_group_wrapping do
-#       control_label_html <<
-#       hidden_field_for_all <<
-#       controls_wrapping do
-#         choice_groups.join("\n").html_safe
-#       end
-#     end
-#   end
-# end
