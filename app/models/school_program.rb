5bbe176282d03b39b2e87f912ece137a2987d41b
Peregrinator <bob.burbach@gmail.com>
2015-09-01 17:13:39 -0700

    Add a method that allows us to ask which attributes have been customized for a particular school_program (rather than inherited from associated community program).

--- app/models/school_program.rb
+++ app/models/school_program.rb
@@ -19,4 +19,16 @@ class SchoolProgram < ActiveRecord::Base
     :service_times,
     :student_population,
     to: :community_program
+  # attributes uniquely assigned to this school program as distinct from
+  # parent community program
+  def customized_attributes
+    return @customized_attributes if @customized_attributes
+
+    @customized_attributes = []
+    delegated_if_blank_methods.each do |method|
+      @customized_attributes << method unless delegating?(method)
+    end
+
+    @customized_attributes
+  end
 end
