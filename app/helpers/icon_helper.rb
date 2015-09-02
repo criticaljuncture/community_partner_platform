8d35774e783f51b3a90f5754a47f9c597021c8c2
Peregrinator <bob.burbach@gmail.com>
2015-09-01 17:20:21 -0700

    Support multiple classes in the icon link to helper to better support toggling (one of them would be hidden by css in the toggle scenario).

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -7,11 +7,18 @@ module IconHelper
 
   def link_to_gicon(text, url, options = {})
     icon_class = options.delete(:icon)
-
     raise ":icon required for link_to_icon" unless icon_class
 
+    # support multiple icons - usually the user will set one of them to
+    # hidden for something like a toggle action
+    icon_classes = icon_class.split(' ')
+
+    wrapper_span = options.fetch(:wrapper_span) { false }
+    text = wrapper_span ?
+      "<span class='text' id='#{wrapper_span[:id]}'>#{text}</span>" : text
+
     link_to(url, options) do
-      "#{gicon(icon_class)} #{text}".html_safe
+      "#{icon_classes.map{|i| gicon(i)}.join(' ')} #{text}".html_safe
     end
   end
 end
