23de99ab0441837163a8572410b818a177bcc0fd
Peregrinator <bob.burbach@gmail.com>
2015-09-01 17:21:23 -0700

    Remove old associations

--- app/audits/community_program_audit.rb
+++ app/audits/community_program_audit.rb
@@ -6,8 +6,6 @@ module CommunityProgramAudit
     {
       primary_quality_element_id: primary_quality_element.id,
       primary_service_type_ids: primary_service_type_ids,
-      secondary_quality_element_id: secondary_quality_element.try(:id),
-      secondary_service_types_ids: secondary_service_type_ids,
       ethnicity_culture_group_ids: ethnicity_culture_groups.map(&:id),
       demographic_group_ids: demographic_groups.map(&:id),
       grade_level_ids: grade_levels.map(&:id),
@@ -21,8 +19,5 @@ module CommunityProgramAudit
     organization.update_version if organization_id_changed?
 
     user.update_version if user_id_changed?
-    school_user.update_version if (user && school_user) &&
-                                    user.id != school_user.id &&
-                                    school_user_id_changed?
   end
 end
