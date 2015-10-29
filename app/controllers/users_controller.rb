ff30e65ecc5aeca21683e18e1d2ba186dfbca3be
Peregrinator <bob.burbach@gmail.com>
2015-10-29 10:34:28 -0700

    Only use user params if they are being passed (as in the case of an ajax user creation from a program form).

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,12 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user = User.new(user_params)
+    if params[:user]
+      @user = User.new(user_params)
+    else
+      @user = User.new
+    end
+
     authorize! :new, @user
 
     if request.xhr?
