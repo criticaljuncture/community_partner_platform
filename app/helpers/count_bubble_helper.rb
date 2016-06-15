af8045bd8f94c464009ccc42eb1766e60aa791d2
Brandon Carag <brandon.carag@gmail.com>
2016-06-15 14:41:29 -0700

    Modify count bubble to display exclamation point in lieu of count on nav tabs to minimize user confusion.

--- app/helpers/count_bubble_helper.rb
+++ app/helpers/count_bubble_helper.rb
@@ -3,4 +3,10 @@ module CountBubbleHelper
     class_name = "count-bubble #{count >= 10 ? 'double-digit' : ''}"
     content_tag :span, count, class: class_name 
   end
+
+  def exclamation_bubble(count)
+    if count > 0
+      content_tag :span, gicon('exclamation-sign'), class: "exclamation-bubble"
+    end
+  end
 end
