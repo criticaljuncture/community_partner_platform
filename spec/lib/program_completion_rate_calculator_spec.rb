7772aee4bd7d73782eb8923e0dbd471e2278e759
Brandon Carag <brandon.carag@gmail.com>
2016-06-17 08:10:11 -0700

    Update specs and change school_program completion ratio calculation slightly to be more explicitly parallel to the community_program completion ratio.

--- spec/lib/program_completion_rate_calculator_spec.rb
+++ spec/lib/program_completion_rate_calculator_spec.rb
@@ -36,5 +36,34 @@ describe "ProgramCompletionRateCalculator" do
 
       expect(calculator.completion_rate).to eq(25.0)
     end
+
+    it "calculates the same rate for community programs and school programs for when shared attributes are populated (apart from #school_programs and #community_program, respectively)." do
+      community_program = double
+      community_program_attributes = CommunityProgram::COMPLETION_WEIGHTS.map{|x| x[1]}.flatten
+      community_program_attributes.each{|x| community_program.stub(x)}
+      community_program.stub(:name).and_return("Community Program")
+      community_program.stub(:student_population).and_return([1])
+      community_program.stub(:school_programs).and_return([1])
+
+      community_program_completion_rate = ProgramCompletionRateCalculator.new(
+        community_program,
+        CommunityProgram::COMPLETION_WEIGHTS
+      ).completion_rate
+
+      school_program = double
+      school_program_attributes = SchoolProgram::COMPLETION_WEIGHTS.map{|x| x[1]}.flatten
+      school_program_attributes.each{|x| school_program.stub(x) }
+      school_program.stub(:name).and_return("School Program")
+      school_program.stub(:student_population).and_return([1])
+      school_program.stub(:community_program).and_return([1])
+
+      school_program_completion_rate = ProgramCompletionRateCalculator.new(
+        school_program,
+        SchoolProgram::COMPLETION_WEIGHTS
+      ).completion_rate
+
+      expect(community_program_completion_rate).to eq(school_program_completion_rate)
+    end
+
   end
 end
