7377779bbcefa863db4cb47a7a28d0804f6192b5
Brandon Carag <brandon.carag@gmail.com>
2016-07-11 16:59:37 -0700

    Require no authentication for public pages and skip cancan authorization requirement.  Add automatic redirect if a current user exists.

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,4 +1,13 @@
 class Public::ApplicationController < ApplicationController
   skip_authorization_check
+  skip_before_filter :authenticate_user!
+  before_filter :redirect_if_no_authentication
+
+  def redirect_if_no_authentication
+    if current_user
+      flash[:alert] = I18n.t("devise.failure.already_authenticated")
+      redirect_to after_sign_in_path_for(current_user)
+    end
+  end
 
 end
