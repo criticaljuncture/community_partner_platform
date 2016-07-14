cb3c68203348e3053e566168a027e210f8034095
Brandon Carag <brandon.carag@gmail.com>
2016-07-14 16:53:46 -0700

    Modify tooltip description to include program count and element breakdown.

--- app/serializers/community_program_marker_serializer.rb
+++ app/serializers/community_program_marker_serializer.rb
@@ -9,7 +9,12 @@ class CommunityProgramMarkerSerializer < ActiveModel::Serializer
     {
       "programCount": object.school_programs.size,
       "url": "http://www.google.com",
-      "description": object.name,
+      "description": "<h1>#{object.name}</h1>
+        <dl>
+          <dt>Total Program Count</dt>
+          <dd>#{object.school_programs.size}</dd>
+          #{school_programs_by_element}
+        </dl>"
     }
   end
 
@@ -18,6 +23,21 @@ class CommunityProgramMarkerSerializer < ActiveModel::Serializer
       "type": "Point",
       "coordinates": [object.lat, object.lng]
     }
+  end
+
+  private
+
+  def school_programs_by_element
+    object.
+      school_programs.
+      reject{|sp| sp.quality_element.nil?}.
+      group_by(&:quality_element).
+      sort_by{|qe, p| qe.name}.map do |quality_element, school_programs|
+        "<dt>#{quality_element.name}</dt>
+         <dd>#{school_programs.count}</dd>
+        "
+      end.
+      join
   end
 
 end
