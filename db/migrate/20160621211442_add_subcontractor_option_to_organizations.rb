b82f3f91f10b7b2933dc0efe213424c02edf8dc4
Peregrinator <bob.burbach@gmail.com>
2016-08-19 12:02:34 -0700

    Add subcontractor field to org show page (district internals). Reduce noise by not bothering to show unless it’s true. Default to false in migration

--- db/migrate/20160621211442_add_subcontractor_option_to_organizations.rb
+++ db/migrate/20160621211442_add_subcontractor_option_to_organizations.rb
@@ -1,5 +1,5 @@
 class AddSubcontractorOptionToOrganizations < ActiveRecord::Migration
   def change
-    add_column :organizations, :subcontractor_with_lead_agency, :boolean
+    add_column :organizations, :subcontractor_with_lead_agency, :boolean, default: false
   end
 end
