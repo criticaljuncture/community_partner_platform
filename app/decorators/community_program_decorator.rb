65361f5e40368bb937b4b6cb90d17a30dba1d6e2
Peregrinator <bob.burbach@gmail.com>
2016-08-19 14:48:39 -0700

    Remove unused method

--- app/decorators/community_program_decorator.rb
+++ app/decorators/community_program_decorator.rb
@@ -27,30 +27,6 @@ class CommunityProgramDecorator < Draper::Decorator
     end.sort_by{|s| s.school.name}
   end
 
-  def school_program_summary
-    #TODO: Someday consider refactoring #school_program_summary and #school_summary together.
-    h.content_tag(:ul, class: 'no-bullets') do
-      schools_with_differing_completion_rates.to_a[0..1].each do |school_program|
-        h.concat h.content_tag(
-          :li,
-          "#{school_program.school.name} (#{h.number_to_percentage(school_program.program_completion_rate, precision: 0)})"
-        )
-      end
-
-      if schools_with_differing_completion_rates.count > 2
-        schools_with_differing_completion_rates[2..-1].each do |school_program|
-          h.concat h.content_tag(:li, "#{school_program.school.name} (#{h.number_to_percentage(school_program.program_completion_rate, precision: 0)})", class: 'hidden overflow')
-        end
-
-        h.concat(
-          h.content_tag(:li, class: 'toggler') do
-            h.concat h.content_tag(:a, "view #{h.pluralize schools_with_differing_completion_rates.count-2, 'other'}")
-          end
-        )
-      end
-    end
-  end
-
   def verification_header
     "#{should_verify?(h.current_user) ? 'Verify' : 'Edit'} #{name}"
   end
