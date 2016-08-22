f037d8313554c1a3bea2609d83d04cb5ecaace2a
Peregrinator <bob.burbach@gmail.com>
2016-08-22 15:31:49 -0700

    Consolidate help hover logic

--- app/helpers/contextual_help_helper.rb
+++ app/helpers/contextual_help_helper.rb
@@ -1,9 +1,26 @@
 module ContextualHelpHelper
-  def help(description)
+  def help(description, options={})
     content_tag :div,
-      class: 'contextual-help cj-tooltip',
-      data: {tooltip: description} do
+      class: "contextual-help cj-tooltip",
+      data: {
+        tooltip: description,
+        'tooltip-classname': options.delete(:class)
+      } do
       gicon 'question-sign'
     end
+  end
+
+  def help_hover(options={}, &block)
+    gravity   = options.fetch(:gravity) { 's' }
+    classname = options.fetch(:classname) { 'help-hover-tooltip' }
+
+    help_hover = <<-HTML
+      <span class="help-hover icon-cpp-help"
+        data-tooltip-content="#{capture(&block)}"
+        data-tooltip-gravity="#{gravity}"
+        data-tooltip-classname="#{classname}"></span>
+    HTML
+
+    help_hover.html_safe
   end
 end
