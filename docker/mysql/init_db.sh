eed73ea32c178832925efbbeb16a5e6bf2f1e83b
Peregrinator <bob.burbach@gmail.com>
2016-10-18 13:35:01 -0700

    Update docker settings

--- docker/mysql/init_db.sh
+++ docker/mysql/init_db.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-
-mysqladmin -u root create $MYSQL_PORT_3306_TCP_DATABASE
-gzip -cd /current.sql.gz | mysql -u root $MYSQL_PORT_3306_TCP_DATABASE
+echo "INIT!!!!!!"
+for i in $MYSQL_PORT_3306_TCP_DATABASE; do
+  if mysqlshow -uroot $i | grep -v Wildcard | grep -o $i --quiet; then
+    echo "Info: Database $i exists, doing nothing"
+  else
+    echo "Info: Database $i does not exist, creating and loading data"
+    mysqladmin -u root create $i
+    gzip -cd ${i}_current.sql.gz | mysql -u root $i
+  fi
+done
