226a97b7247ecea81e760a623aae219acfd40093
Peregrinator <bob.burbach@gmail.com>
2017-01-12 11:50:19 -0800

    Update redis to use ENV vars for connection settings and update properly when passenger forks.

--- config/initializers/_redis.rb
+++ config/initializers/_redis.rb
@@ -1,1 +1,17 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+REDIS_CONNECTION_SETTINGS = {
+  db: ENV["REDIS_DB"] || 0,
+  host: ENV["REDIS_HOST"] || '127.0.0.1',
+  port: ENV["REDIS_PORT"] || 6379
+}
+
+$redis = Redis.new(REDIS_CONNECTION_SETTINGS)
+
+if defined?(PhusionPassenger)
+  PhusionPassenger.on_event(:starting_worker_process) do |forked|
+    # We're in smart spawning mode.
+    if forked
+      $redis.client.disconnect
+      $redis = Redis.new(REDIS_CONNECTION_SETTINGS)
+    end
+  end
+end
