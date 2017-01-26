aa746cbb88087b80c781d966f64b702367f11acb
Peregrinator <bob.burbach@gmail.com>
2017-01-26 14:56:30 -0800

    Updates for docker

--- config/initializers/_redis.rb
+++ config/initializers/_redis.rb
@@ -1,7 +1,7 @@
 REDIS_CONNECTION_SETTINGS = {
   db: ENV["REDIS_DB"] || 0,
-  host: ENV["REDIS_HOST"] || '127.0.0.1',
-  port: ENV["REDIS_PORT"] || 6379
+  host: ENV["REDIS_PORT_6379_TCP_ADDR"] || '127.0.0.1',
+  port: ENV["REDIS_PORT_6379_TCP_PORT"] || 6379
 }
 
 $redis = Redis.new(REDIS_CONNECTION_SETTINGS)
