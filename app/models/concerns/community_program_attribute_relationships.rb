012e5dc236be167f3ec23a7327355354d350cf81
Peregrinator <bob.burbach@gmail.com>
2017-01-26 17:24:34 -0800

    Add dependent destroy for associated attributes

--- app/models/concerns/community_program_attribute_relationships.rb
+++ app/models/concerns/community_program_attribute_relationships.rb
@@ -5,24 +5,30 @@ module CommunityProgramAttributeRelationships
     belongs_to :student_population
 
     has_many :community_program_ethnicity_culture_groups,
-      as: :attributable
+      as: :attributable,
+      dependent: :destroy
     has_many :ethnicity_culture_groups, through: :community_program_ethnicity_culture_groups
 
     has_many :community_program_demographic_groups,
-      as: :attributable
+      as: :attributable,
+      dependent: :destroy
     has_many :demographic_groups, through: :community_program_demographic_groups
 
     has_many :community_program_grade_levels,
-      as: :attributable
+      as: :attributable,
+      dependent: :destroy
     has_many :grade_levels, through: :community_program_grade_levels
 
     has_many :community_program_service_days,
-      as: :attributable
+      as: :attributable,
+      dependent: :destroy
     has_many :days, through: :community_program_service_days
 
     has_many :community_program_service_times,
-      as: :attributable
-    has_many :service_times, through: :community_program_service_times
+      as: :attributable,
+      dependent: :destroy
+    has_many :service_times, through: :community_program_service_times,
+      dependent: :destroy
   end
 
   def inheritable_attributes
