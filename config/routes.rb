561fd8b6912ddeafc08eea80bb0bf5cbc32dfd52
Peregrinator <bob.burbach@gmail.com>
2017-02-13 11:04:52 -0800

    Add default_url host settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+Rails.application.routes.default_url_options[:host] = Settings.application.canonical_hostname
+
 OusdCommunityPartners::Application.routes.draw do
   devise_for :users, path: "auth", class_name: 'User', controllers: { registrations: "users/registrations" }
 
