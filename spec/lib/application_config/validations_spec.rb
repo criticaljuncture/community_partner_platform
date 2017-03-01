81b781571e4316fa4d4cb48132fa9bf437f6b048
Peregrinator <bob.burbach@gmail.com>
2017-03-01 15:53:53 -0800

    Test app_config validations independently from org. Test that org includes this module

---
+++ spec/lib/application_config/validations_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+module ApplicationConfig
+  class ValidationTest < Struct.new(:favorite_color, :name)
+    include ActiveModel::Validations
+    include ApplicationConfig::Validations
+  end
+end
+
+RSpec.describe ApplicationConfig::Validations do
+  let(:model) {
+    ApplicationConfig::ValidationTest.new(
+      'blue, no yellow',
+      'Test'
+    )
+  }
+
+  context "validations from app configuration" do
+    it "properly sets them for `presence` type validations" do
+      model.name = nil
+      expect(model).not_to be_valid
+
+      model.name = 'Test'
+      expect(model).to be_valid
+    end
+
+    it "properly sets the error message when provided" do
+      model.favorite_color = nil
+      expect(model).not_to be_valid
+      expect(
+        model.errors.messages[:favorite_color]
+      ).to eq(['Please choose from the list above'])
+
+      model.favorite_color = 'blue, no yellow'
+      expect(model).to be_valid
+    end
+  end
+end
