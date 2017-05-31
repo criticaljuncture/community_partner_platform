16f311bba87620893eaf33f0547bb52e1d7daf7c
Peregrinator <bob.burbach@gmail.com>
2017-05-31 16:27:45 -0700

    Add more flexibility to list display with toggling

--- app/helpers/list_display_helper.rb
+++ app/helpers/list_display_helper.rb
@@ -1,7 +1,18 @@
 module ListDisplayHelper
 
-  def view_more_toggle_list(objects, threshold=2)
-    content_tag(:ul, class: 'no-bullets') do
+  def view_more_toggle_list(objects, threshold: 2, toggler_position: :last, toggler_label: '')
+    content_tag(:ul, class: 'view-more-list no-bullets') do
+      if objects.size > threshold && toggler_position == :first
+        concat(
+          content_tag(:li, class: 'toggler') do
+            if toggler_label.present?
+              concat "#{toggler_label} "
+            end
+            concat content_tag(:a, "view #{objects.size - threshold} more")
+          end
+        )
+      end
+
       objects.to_a[0..threshold-1].each do |obj|
         concat content_tag(:li, yield(obj))
       end
@@ -11,11 +22,16 @@ module ListDisplayHelper
           concat content_tag(:li, yield(obj), class: 'hidden overflow')
         end
 
-        concat(
-          content_tag(:li, class: 'toggler') do
-            concat content_tag(:a, "view #{objects.size-2} more")
-          end
-        )
+        if toggler_position == :last
+          concat(
+            content_tag(:li, class: 'toggler') do
+              if toggler_label.present?
+                concat "#{toggler_label} "
+              end
+              concat content_tag(:a, "view #{objects.size - threshold} more")
+            end
+          )
+        end
       end
     end
   end
