eb8f9c2851fb4cbf62234139cc242025826cc080
Peregrinator <bob.burbach@gmail.com>
2017-06-01 09:37:56 -0700

    Order the school site types in a more ‘human’ way by using a predefined order.

--- app/presenters/public/home_page_presenter.rb
+++ app/presenters/public/home_page_presenter.rb
@@ -1,10 +1,16 @@
 class Public::HomePagePresenter
   def schools
+    ordered_site_types = ["Elementary Schools", "Grades K-8", "Middle Schools", "Grades 6-12", "High Schools"]
+
     @schools ||= SchoolDecorator.decorate_collection(
       School.select(
         :name, :id, :site_type_norm
       ).order(:site_type_norm, :name)
-    ).group_by(&:human_site_type)
+    )
+    .group_by(&:human_site_type)
+    .sort do |site_type_1, site_type_2|
+      ordered_site_types.index(site_type_1[0]) <=> ordered_site_types.index(site_type_2[0])
+    end
   end
 
   def organizations
