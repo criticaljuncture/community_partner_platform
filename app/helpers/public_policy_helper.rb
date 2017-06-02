32251066c0045c09d1be93e8aacfe22ed0b999e1
Peregrinator <bob.burbach@gmail.com>
2017-06-02 11:23:18 -0700

    Add warnings to the organization page to let the user know if a previous authorization for public view is about to expire because the record hasn’t been updated. Add warning to let user know what is required to be valid for public view (when the record isn’t currently valid).

--- app/helpers/public_policy_helper.rb
+++ app/helpers/public_policy_helper.rb
@@ -1,5 +1,9 @@
 module PublicPolicyHelper
   def missing_requirements_tooltip(model)
-    "Can not be made public for the following reasons: <ul><li>#{model.public_policy.missing_requirements.join('</li><li> ')}</li></ul>"
+    "Can not be made public for the following reasons: #{missing_requirements_list(model.public_policy.missing_requirements)}"
+  end
+
+  def missing_requirements_list(missing_requirements)
+    "<ul><li>#{missing_requirements.join('</li><li>')}</li></ul>"
   end
 end
