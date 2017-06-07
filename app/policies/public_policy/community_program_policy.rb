21e514e8e5d58efc3417d93c32d73b7a67cd39bf
Peregrinator <bob.burbach@gmail.com>
2017-06-07 09:58:14 -0700

    Update community program public policy to require that the associated organization has been made public first.

--- app/policies/public_policy/community_program_policy.rb
+++ app/policies/public_policy/community_program_policy.rb
@@ -1,3 +1,30 @@
 class PublicPolicy::CommunityProgramPolicy < PublicPolicy::Base
   policy_for :community_program
+
+    def eligible_to_be_made_public?
+      eligible = required_attributes_present? &&
+        minimally_complete? &&
+          !verification_required?
+
+
+      if User.current && User.current.can?(:make_public, Organization)
+        eligible && model.organization.approved_for_public?
+      else
+        eligible
+      end
+    end
+
+    def missing_requirements
+      missing_requirements = super
+
+      if User.current && User.current.can?(:make_public, Organization)
+        unless model.organization.approved_for_public?
+          missing_requirements << I18n.t(
+            'public_policy.missing_requirements.organization_not_public'
+          )
+        end
+      end
+
+      missing_requirements
+    end
 end
