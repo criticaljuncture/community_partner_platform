544001f3c6b1991dd63ce8adedcf5c50650406eb
Peregrinator <bob.burbach@gmail.com>
2018-07-23 14:45:21 -0700

    Rename session key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-  key: '_ousd_community_partners_session',
+  key: '_community_partner_platform_session',
   # Only send cookie over SSL when in production mode
   secure: Rails.env.production? ? true : false,
   # Don't allow Javascript to access the cookie (mitigates cookie-based XSS exploits)
