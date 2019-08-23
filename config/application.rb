660d2cbcadcfe503d0d036de5d6ff89702be6b2f
Peregrinator <bob.burbach@gmail.com>
2019-08-23 11:12:43 -0700

    Update: Allow for overrides of more text within the application. Add sps specific overrides

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module CommunityPartnerPlatform
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')] - 
+      Dir[Rails.root.join('config', 'locales', 'overrides', '*.{rb,yml}')] +
+      Dir[Rails.root.join('config', 'locales', 'overrides', Settings.application.subdomain, '*.{rb,yml}')]
   end
 end
