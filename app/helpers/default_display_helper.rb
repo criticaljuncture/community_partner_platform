8c74bbd69bb55bac1c9d7d591b66d6180dd963ac
Peregrinator <bob.burbach@gmail.com>
2019-12-16 14:11:20 -0800

    Update: default display helper for blank values to display 'not provided' in tabular layouts

--- app/helpers/default_display_helper.rb
+++ app/helpers/default_display_helper.rb
@@ -2,9 +2,7 @@ module DefaultDisplayHelper
   def default_value_for(model, attr)
     value = model.send(attr)
 
-    if value.nil?
-      ''
-    elsif value.is_a?(String)
+    if value.is_a?(String) && value != ''
       if attr == :url
         link_to value, value, target: :_blank
       else
@@ -14,12 +12,14 @@ module DefaultDisplayHelper
       I18n.t("app.yes")
     elsif value.is_a?(FalseClass)
       I18n.t("app.no")
-    elsif value.respond_to?(:to_a)
+    elsif value && value.respond_to?(:to_a)
       value
         .map{|v| v.respond_to?(:default_display) ? v.default_display : v}
         .join(', ')
     elsif value.respond_to?(:default_display)
       value.default_display
+    elsif value.blank?
+      "<span class='hint'>#{I18n.t("app.not_provided")}</span>".html_safe
     else
       value
     end
